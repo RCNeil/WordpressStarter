4c04628fccdf0794ffc5e5849ec134c9f5ea7076
RCNeil <rneil99@gmail.com>
2020-10-19 13:01:09 -0400

    Updates styles and footer

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="span20 text-center" style="margin:0px auto; float:none;">
-			<a href="http://squatch.us" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/built-by-squatch.png" alt="" class="builtby" /></a>
+		<div class="span20 text-center">
+			<a href="https://squatch.us"  class="builtby" target="_blank"><img src="<?php bloginfo('template_url'); ?>/images/built-by-squatch.png" alt="Built By Squatch Creative" /></a>
 		</div>
 	</div>
 <div class="clear"></div>
