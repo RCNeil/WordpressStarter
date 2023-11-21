12c3e7f38f4fd04219cebe31326c3e63cf9e18e1
Randy Neil <rneil99@gmail.com>
2023-11-21 14:57:30 -0500

    Updating Functions and Social Share

--- functions.php
+++ functions.php
@@ -5,49 +5,19 @@
  *  Custom functions, support, custom post types and more. Modified from HTMLBlank @toddmotto
  */
 
-if (!isset($content_width)) { $content_width = 900;	}
-
 if (function_exists('add_theme_support')){
     add_theme_support('menus');
     add_theme_support('post-thumbnails');
     add_theme_support('automatic-feed-links');
-    load_theme_textdomain('html5blank', get_template_directory() . '/languages');
+	add_theme_support ('align-wide');
+	add_theme_support ('responsive-embeds');
+	add_theme_support('editor-styles');
+	add_editor_style( 'editor-styles.css' );
+    load_theme_textdomain('squatch', get_template_directory() . '/languages');
 }
 
-/*------------------------------------*\
-	Functions
-\*------------------------------------*/
-
-// HTML5 Blank navigation
-function html5blank_nav()
-{
-	wp_nav_menu(
-	array(
-		'theme_location'  => 'header-menu',
-		'menu'            => '',
-		'container'       => 'div',
-		'container_class' => 'menu-{menu slug}-container',
-		'container_id'    => '',
-		'menu_class'      => 'menu',
-		'menu_id'         => '',
-		'echo'            => true,
-		'fallback_cb'     => 'wp_page_menu',
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul>%3$s</ul>',
-		'depth'           => 0,
-		'walker'          => ''
-		)
-	);
-}
-
-// Load HTML5 Blank scripts (header.php)
-function html5blank_header_scripts()
-{
+function squatch_header_scripts() {
     if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
-
     	wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0'); // Conditionizr
         wp_enqueue_script('conditionizr');
 
@@ -61,59 +31,36 @@ function html5blank_header_scripts()
 		//wp_enqueue_script('slick', get_template_directory_uri() . '/js/lib/slick.min.js', array('jquery'), '1', true); 
 		wp_enqueue_script( 'inview', get_stylesheet_directory_uri() . '/js/lib/jquery.inview.min.js', array( 'jquery' ), '1', true );
 
-        wp_register_script('html5blankscripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); // Custom scripts
-        wp_enqueue_script('html5blankscripts'); 
+        wp_register_script('squatch-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0'); 
+        wp_enqueue_script('squatch-scripts'); 
     }
 }
 
-// Load HTML5 Blank styles
-function html5blank_styles()
-{
+function squatch_styles() {
     wp_register_style('normalize', get_template_directory_uri() . '/normalize.css', array(), '1.0', 'all');
-    wp_enqueue_style('normalize');
-	
+    wp_enqueue_style('normalize');	
 	//wp_enqueue_style( 'fancybox', get_stylesheet_directory_uri() . '/js/lib/jquery.fancybox.min.css' );
 	
 	//wp_enqueue_style( 'slick', get_template_directory_uri() . '/js/lib/slick.css' );
 	
 	wp_enqueue_style( 'font-awesome', 'https://use.fontawesome.com/releases/v5.10.0/css/all.css' );
 
-    wp_register_style('html5blank', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
-    wp_enqueue_style('html5blank');
+    wp_register_style('squatch-styles', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('squatch-styles');
 }
 
 
-function register_html5_menu()
-{
+function register_menu_positions() {
     register_nav_menus(array( 
-        'header-menu' => __('Header Menu', 'html5blank'),
-        'sidebar-menu' => __('Sidebar Menu', 'html5blank'), 
-        'footer-menu' => __('Footer Menu', 'html5blank') 
+        'header-menu' => __('Header Menu', 'squatch'),
+        'sidebar-menu' => __('Sidebar Menu', 'squatch'), 
+        'footer-menu' => __('Footer Menu', 'squatch') 
     ));
 }
 
-// Remove the <div> surrounding the dynamic navigation to cleanup markup
-function my_wp_nav_menu_args($args = '')
-{
-    $args['container'] = false;
-    return $args;
-}
-
-// Remove Injected classes, ID's and Page ID's from Navigation <li> items
-function my_css_attributes_filter($var)
-{
-    return is_array($var) ? array() : '';
-}
-
-// Remove invalid rel attribute values in the categorylist
-function remove_category_rel_from_category_list($thelist)
-{
-    return str_replace('rel="category tag"', 'rel="tag"', $thelist);
-}
 
 // Add page slug to body class, love this - Credit: Starkers Wordpress Theme
-function add_slug_to_body_class($classes)
-{
+function add_slug_to_body_class($classes) {
     global $post;
     if (is_home()) {
         $key = array_search('blog', $classes);
@@ -125,126 +72,35 @@ function add_slug_to_body_class($classes)
     } elseif (is_singular()) {
         $classes[] = sanitize_html_class($post->post_name);
     }
-
     return $classes;
 }
 
-// If Dynamic Sidebar Exists
-if (function_exists('register_sidebar'))
-{
-    // Define Sidebar Widget Area 1
+//Dynamic Widget Area
+if (function_exists('register_sidebar')) {
     register_sidebar(array(
-        'name' => __('Widget Area 1', 'html5blank'),
-        'description' => __('Description for this widget-area...', 'html5blank'),
+        'name' => __('Widget Area 1', 'squatch'),
+        'description' => __('Description for this widget-area...', 'squatch'),
         'id' => 'widget-area-1',
         'before_widget' => '<div id="%1$s" class="%2$s">',
         'after_widget' => '</div>',
         'before_title' => '<h3>',
         'after_title' => '</h3>'
     ));
-
-    // Define Sidebar Widget Area 2
-    register_sidebar(array(
-        'name' => __('Widget Area 2', 'html5blank'),
-        'description' => __('Description for this widget-area...', 'html5blank'),
-        'id' => 'widget-area-2',
-        'before_widget' => '<div id="%1$s" class="%2$s">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ));
-}
-
-// Remove wp_head() injected Recent Comment styles
-function my_remove_recent_comments_style()
-{
-    global $wp_widget_factory;
-    remove_action('wp_head', array(
-        $wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
-        'recent_comments_style'
-    ));
-}
-
-// Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
-function html5wp_pagination()
-{
-    global $wp_query;
-    $big = 999999999;
-    echo paginate_links(array(
-        'base' => str_replace($big, '%#%', get_pagenum_link($big)),
-        'format' => '?paged=%#%',
-        'current' => max(1, get_query_var('paged')),
-        'total' => $wp_query->max_num_pages
-    ));
-}
-
-// Custom Excerpts
-function html5wp_index($length) // Create 20 Word Callback for Index page Excerpts, call using html5wp_excerpt('html5wp_index');
-{
-    return 20;
-}
-
-// Create 40 Word Callback for Custom Post Excerpts, call using html5wp_excerpt('html5wp_custom_post');
-function html5wp_custom_post($length)
-{
-    return 40;
-}
-
-// Create the Custom Excerpts callback
-function html5wp_excerpt($length_callback = '', $more_callback = '')
-{
-    global $post;
-    if (function_exists($length_callback)) {
-        add_filter('excerpt_length', $length_callback);
-    }
-    if (function_exists($more_callback)) {
-        add_filter('excerpt_more', $more_callback);
-    }
-    $output = get_the_excerpt();
-    $output = apply_filters('wptexturize', $output);
-    $output = apply_filters('convert_chars', $output);
-    $output = '<p>' . $output . '</p>';
-    echo $output;
-}
-
-// Custom View Article link to Post
-function html5_blank_view_article($more)
-{
-    global $post;
-   // return '... <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('View Article', 'html5blank') . '</a>';
-    return '...';
 }
 
 // Remove Admin bar
-function remove_admin_bar()
-{
+function remove_admin_bar() {
     return false;
 }
 
 // Remove 'text/css' from our enqueued stylesheet
-function html5_style_remove($tag)
-{
+function html5_style_remove($tag) {
     return preg_replace('~\s+type=["\'][^"\']++["\']~', '', $tag);
 }
 
-// Remove thumbnail width and height dimensions that prevent fluid images in the_thumbnail
-function remove_thumbnail_dimensions( $html )
-{
-    $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html);
-    return $html;
-}
-
-// Custom Gravatar in Settings > Discussion
-function html5blankgravatar ($avatar_defaults)
-{
-    $myavatar = get_template_directory_uri() . '/img/gravatar.jpg';
-    $avatar_defaults[$myavatar] = "Custom Gravatar";
-    return $avatar_defaults;
-}
 
 // Threaded Comments
-function enable_threaded_comments()
-{
+function enable_threaded_comments() {
     if (!is_admin()) {
         if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
             wp_enqueue_script('comment-reply');
@@ -252,61 +108,15 @@ function enable_threaded_comments()
     }
 }
 
-// Custom Comments Callback
-function html5blankcomments($comment, $args, $depth)
-{
-	$GLOBALS['comment'] = $comment;
-	extract($args, EXTR_SKIP);
-
-	if ( 'div' == $args['style'] ) {
-		$tag = 'div';
-		$add_below = 'comment';
-	} else {
-		$tag = 'li';
-		$add_below = 'div-comment';
-	}
-?>
-    <!-- heads up: starting < for the html tag (li or div) in the next line: -->
-    <<?php echo $tag ?> <?php comment_class(empty( $args['has_children'] ) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
-	<?php if ( 'div' != $args['style'] ) : ?>
-	<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-	<?php endif; ?>
-	<div class="comment-author vcard">
-	<?php if ($args['avatar_size'] != 0) echo get_avatar( $comment, $args['180'] ); ?>
-	<?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()) ?>
-	</div>
-<?php if ($comment->comment_approved == '0') : ?>
-	<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.') ?></em>
-	<br />
-<?php endif; ?>
-
-	<div class="comment-meta commentmetadata"><a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ) ?>">
-		<?php
-			printf( __('%1$s at %2$s'), get_comment_date(),  get_comment_time()) ?></a><?php edit_comment_link(__('(Edit)'),'  ','' );
-		?>
-	</div>
-
-	<?php comment_text() ?>
-
-	<div class="reply">
-	<?php comment_reply_link(array_merge( $args, array('add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
-	</div>
-	<?php if ( 'div' != $args['style'] ) : ?>
-	</div>
-	<?php endif; ?>
-<?php }
-
 /*------------------------------------*\
 	Actions + Filters + ShortCodes
 \*------------------------------------*/
 
 // Add Actions
-add_action('init', 'html5blank_header_scripts'); 
+add_action('init', 'squatch_header_scripts'); 
 add_action('get_header', 'enable_threaded_comments'); 
-add_action('wp_enqueue_scripts', 'html5blank_styles');
-add_action('init', 'register_html5_menu');
-add_action('widgets_init', 'my_remove_recent_comments_style');
-add_action('init', 'html5wp_pagination');
+add_action('wp_enqueue_scripts', 'squatch_styles');
+add_action('init', 'register_menu_positions');
 // Remove Actions
 remove_action('wp_head', 'feed_links_extra', 3); 
 remove_action('wp_head', 'feed_links', 2);
@@ -322,19 +132,13 @@ remove_action('wp_head', 'rel_canonical');
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 
 // Add Filters
-add_filter('avatar_defaults', 'html5blankgravatar'); 
 add_filter('body_class', 'add_slug_to_body_class');
 add_filter('widget_text', 'do_shortcode');
 add_filter('widget_text', 'shortcode_unautop'); 
-add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args');
-add_filter('the_category', 'remove_category_rel_from_category_list');
 add_filter('the_excerpt', 'shortcode_unautop'); 
 add_filter('the_excerpt', 'do_shortcode'); 
-add_filter('excerpt_more', 'html5_blank_view_article');
 add_filter('show_admin_bar', 'remove_admin_bar');
 add_filter('style_loader_tag', 'html5_style_remove'); 
-add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10);
-add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10);
 // Remove Filters
 remove_filter('the_excerpt', 'wpautop'); 
 
@@ -363,11 +167,6 @@ function remove_menus(){
 }
 add_action( 'admin_menu', 'remove_menus', 999 );
 
-//CUSTOM ACF MIN HEIGHT ROWS
-add_filter('admin_head','textarea_temp_fix');
-function textarea_temp_fix() {
-	echo '<style type="text/css">.acf_postbox .field textarea {min-height:0 !important;}</style>';
-}
 //SQUATCH WP LOGO
 function add_squatch_logo() { ?>
     <style type="text/css">
@@ -382,33 +181,6 @@ function add_squatch_logo() { ?>
 <?php }
 add_action( 'login_enqueue_scripts', 'add_squatch_logo' );
 
-//ALLOW WEBP
-function webp_upload_mimes($existing_mimes) {
-    $existing_mimes['webp'] = 'image/webp';
-    return $existing_mimes;
-}
-add_filter('mime_types', 'webp_upload_mimes');
-function webp_is_displayable($result, $path) {
-    if ($result === false) {
-        $displayable_image_types = array( IMAGETYPE_WEBP );
-        $info = @getimagesize( $path );
-        if (empty($info)) {
-            $result = false;
-        } elseif (!in_array($info[2], $displayable_image_types)) {
-            $result = false;
-        } else {
-            $result = true;
-        }
-    }
-    return $result;
-}
-add_filter('file_is_displayable_image', 'webp_is_displayable', 10, 2);
-
-
-add_theme_support ('align-wide');
-add_theme_support ('responsive-embeds');
-add_theme_support('editor-styles');
-add_editor_style( 'editor-styles.css' );
 function squatch_custom_block_categories( $categories ) {
 	$custom_category = array(
 		'slug'  => 'squatch-blocks',
@@ -452,27 +224,24 @@ function my_acf_init_block_types() {
 	}
 }
 
-
-
-
 /*
 function create_post_type() {
 
     register_post_type('new-post-type', // Register Custom Post Type
         array(
         'labels' => array(
-            'name' => __('New Post Type', 'html5blank'), // Rename these to suit
-            'singular_name' => __('New Post Type', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New ', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit ', 'html5blank'),
-            'new_item' => __('New ', 'html5blank'),
-            'view' => __('View ', 'html5blank'),
-            'view_item' => __('View ', 'html5blank'),
-            'search_items' => __('Search ', 'html5blank'),
-            'not_found' => __('No posts found', 'html5blank'),
-            'not_found_in_trash' => __('No posts found in Trash', 'html5blank')
+            'name' => __('New Post Type', 'squatch'), // Rename these to suit
+            'singular_name' => __('New Post Type', 'squatch'),
+            'add_new' => __('Add New', 'squatch'),
+            'add_new_item' => __('Add New ', 'squatch'),
+            'edit' => __('Edit', 'squatch'),
+            'edit_item' => __('Edit ', 'squatch'),
+            'new_item' => __('New ', 'squatch'),
+            'view' => __('View ', 'squatch'),
+            'view_item' => __('View ', 'squatch'),
+            'search_items' => __('Search ', 'squatch'),
+            'not_found' => __('No posts found', 'squatch'),
+            'not_found_in_trash' => __('No posts found in Trash', 'squatch')
         ),
         'public' => true,
 		'rewrite' => array( 'slug' => 'new-post-type' ),
--- includes/social-share.php
+++ includes/social-share.php
@@ -1,8 +1,7 @@
-<div class="clear"></div>
 <ul class="social-share">
-	<li class="share-title">Share This:</li>
-	<li><a href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo get_the_permalink(); ?>&amp;title=<?php echo get_the_title(); ?>" class="social-refer" title="Share On LinkedIn" target="_blank"><i class="fa fa-linkedin-square"></i></a></li>
-	<li><a href="https://www.facebook.com/share.php?u=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="social-refer" title="Share on Facebook" target="_blank"><i class="fab fa-facebook-square"></i></a></li>
-	<li><a href="https://twitter.com/home?status=<?php echo get_the_title(); ?>+<?php echo get_the_permalink(); ?>" class="social-refer" title="Share on Twitter" target="_blank"><i class="fab fa-twitter-square"></i></a></li>
+	<li class="share-title">Share</li>
+	<li><a href="https://twitter.com/intent/tweet?counturl=<?php echo get_the_permalink(); ?>&text=<?php echo get_the_title(); ?>&url=<?php echo get_the_permalink(); ?>" class="social-refer twitter" target="_blank"><i class="fab fa-twitter"></i> Twitter</a></li>
+	<li><a href="https://www.facebook.com/share.php?u=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="social-refer facebook" target="_blank"><i class="fab fa-facebook-f"></i> Facebook</a></li>
+	<li><a href="https://www.reddit.com/submit?url=<?php echo get_the_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="social-refer reddit" target="_blank"><i class="fab fa-reddit-alien"></i> Reddit</a></li>
+	<li><a href="http://pinterest.com/pin/create/link/?url=<?php echo get_the_permalink(); ?>" class="social-refer pinterest" target="_blank"><i class="fab fa-pinterest"></i> Pinterest</a></li>
 </ul>
-<div class="clear"></div>
