cc96b1efd0be49f622dcca6a7c0c5206c753bb5c
RCNeil <rneil99@gmail.com>
2025-07-16 12:14:06 -0400

    Removed Conditionizr and Favicon Tags
    Since Wordpress uses the Site Icon setting, we no longer need to set the favicon manually

--- functions.php
+++ functions.php
@@ -30,8 +30,7 @@ if (function_exists('add_theme_support')){
 
 function squatch_header_scripts() {
     if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
-    	wp_register_script('conditionizr', get_template_directory_uri() . '/js/lib/conditionizr-4.3.0.min.js', array(), '4.3.0'); // Conditionizr
-        wp_enqueue_script('conditionizr');
+
 
         wp_register_script('modernizr', get_template_directory_uri() . '/js/lib/modernizr-2.7.1.min.js', array(), '2.7.1'); // Modernizr
         wp_enqueue_script('modernizr');
--- header.php
+++ header.php
@@ -4,26 +4,10 @@
 <meta charset="<?php bloginfo('charset'); ?>">
 <title><?php wp_title(''); ?></title>
 
-<link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/images/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="<?php bloginfo('template_url'); ?>/images/favicon.svg" />
-<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
-<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/images/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-title" content="<?php bloginfo('name'); ?>" />
-<link rel="manifest" href="<?php bloginfo('template_url'); ?>/images/site.webmanifest" />
-<link href="<?php bloginfo('template_url'); ?>/images/favicon.ico" rel="icon" type="image/x-icon">
-
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <?php wp_head(); ?>
-<script>
-// conditionizr.com
-// configure environment tests
-conditionizr.config({
-assets: '<?php echo get_template_directory_uri(); ?>',
-tests: {}
-});
-</script>
 
 </head>
 <body <?php body_class(); ?>>
