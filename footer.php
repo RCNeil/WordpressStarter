097ef68d44e5c8495efd48392878a999374d0ce3
RCNeil <rneil99@gmail.com>
2025-11-13 13:47:19 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,18 +2,11 @@
 <footer>
 <div class="footer container" role="contentinfo">
 	<div class="row">
-		<div class="span100">
-			<p class="copyright">&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.</p>
-		</div>
+		<p class="copyright">&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.</p>
+		<a href="https://squatch.us"  class="builtby" target="_blank"  title="Built By Squatch Creative">
+			<?php get_template_part('images/built-by-squatch'); ?>
+		</a>
 	</div>
-	<div class="row">
-		<div class="span20 text-center">
-			<a href="https://squatch.us"  class="builtby" target="_blank"  title="Built By Squatch Creative">
-				<?php get_template_part('images/built-by-squatch'); ?>
-			</a>
-		</div>
-	</div>
-<div class="clear"></div>
 </div>
 </footer>
 <!-- /footer -->
